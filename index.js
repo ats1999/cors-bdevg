df9b099727a683283d7954d3d60a63429d0491fa
Rahul kumar <rkt82224@gmail.com>
2020-11-17 03:55:35 +1100

    test

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ app.use(cors())
 app.get('/test', (req, res) => {
   res.send('Hello World of cors!')
 })
-
+app.get('/', (req, res) => {
+  res.send('Hello World home!')
+})
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
