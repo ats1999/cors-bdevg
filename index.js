0270ee2b746a08e223d2e1dea9aad2a554a68a9e
Rahul kumar <rkt82224@gmail.com>
2020-12-03 21:09:41 +1100

    changed route

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ var cors = require('cors')
 const port = 4000
 app.use(cors())
 app.get('/test', (req, res) => {
-  res.send('Hello World of cors!')
+  res.send('<h1>Testing cors issue!</h1>')
 })
 app.get('/', (req, res) => {
-  res.send('Hello World home!')
+  res.send('<h1>Hello micro world!</h1>')
 })
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
