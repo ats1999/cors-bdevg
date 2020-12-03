fbf2e79013864a7e86bba1cc17864022cfa5ee99
Rahul kumar <rkt82224@gmail.com>
2020-12-03 21:35:27 +1100

    changed html

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ var cors = require('cors')
 const port = 4000
 app.use(cors())
 app.get('/test', (req, res) => {
-  res.send('<h1>Testing cors issue!</h1>')
+  res.send('Testing cors issue!')
 })
 app.get('/', (req, res) => {
-  res.send('<h1>Hello micro world!</h1>')
+  res.send('Hello micro world!')
 })
 app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening at http://localhost:${process.env.PORT || port}`)
